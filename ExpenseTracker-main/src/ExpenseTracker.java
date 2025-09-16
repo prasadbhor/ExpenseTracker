69e4ff759e88ed60731bea389339e67009d4ba09
Prasad Bhor <prasadbhor0767@gmail.com>
2025-09-16 17:27:06 +0530

    Change in line 25 Prasad Bhor Hi

--- ExpenseTracker-main/src/ExpenseTracker.java
+++ ExpenseTracker-main/src/ExpenseTracker.java
@@ -22,7 +22,7 @@ public class ExpenseTracker {
 
     private static void printWelcome() {
         System.out.println("===============PrasadBhor=================");
-        System.out.println("         Welcome to Expense Tracker  System  Prasad Bhor Hi     ");
+        System.out.println("         Welcome to Expense Tracker  System      ");
         System.out.println("=================Assignment===============");
     }
 
